eeb0e88ec2f5111c7563fbf4fb64b1041bd676a0
Ye Min Aung <yeminaung013@gmail.com>
2023-07-30 13:10:07 +0630

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*.html", "./public/*.html"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                ubuntu: "#822748",
+            },
+        },
+    },
+    plugins: [],
+};
